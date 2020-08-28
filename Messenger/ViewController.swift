989e6c9eec2eaa4730761f22989bde04dffb7f6c
dewa <dewaprabawa>
2020-08-28 18:14:56 +0800

    first commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Dewa Prabawa on 28/08/20.
+//  Copyright © 2020 Dewa Prabawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
