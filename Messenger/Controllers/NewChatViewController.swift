f1c3b2fcbda3040b6ce8d3968c8dc2020e712687
dewa <dewaprabawa>
2020-08-28 18:35:15 +0800

    wrap up setup app folder

---
+++ Messenger/Controllers/NewChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewChatViewController.swift
+//  Messenger
+//
+//  Created by Dewa Prabawa on 28/08/20.
+//  Copyright © 2020 Dewa Prabawa. All rights reserved.
+//
+
+import UIKit
+
+class NewChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
